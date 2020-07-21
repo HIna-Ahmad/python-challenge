f1ea2adf5587bcfb1aa735a9be01c2c011a3684d
Hina Ahmad <hinaahmad93@gmail.com>
2020-07-20 22:53:25 -0400

    going to get an A

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -28,14 +28,16 @@ with open("python-challenge/PyPoll/Resources/election_data.csv") as data:
         name = str(row[2])
         ballot_ct += 1
         
+        
         if not name in candidate_count:
                 candidate_count[name] = 1
         else:
             candidate_count[name] += 1
-      
-elections =list(set(candidate_emptylist))
+out_put1 = (f"Election Results\n" + f"-----------------\n" + f"Total Votes: {ballot_ct}\n")
+elections = list(set(candidate_emptylist))
 elections.sort()      
 vote_count = []
+
 
 for hina in elections:
     vote_count.append(candidate_emptylist.count(hina))
@@ -45,28 +47,26 @@ for hina in elections:
         candidate_info[name] = round((candidate_count[name] / ballot_ct) * 100)
 winner = elections[vote_count.index(max(vote_count))]
 khan = winner
-        
 
 
 print("Election Results")
 print("--------------------")
 print(f"Total Votes: {ballot_ct}")
 print("--------------------")
+election_results = ""
 
 for name in candidate_count:
-    print(f"{name}: {candidate_info[name]}%  {candidate_count[name]}")
+    candidate_results = (f"{name}: {candidate_info[name]}%  {candidate_count[name]}\n")
+    election_results += candidate_results
+    
+print(election_results)
 print("--------------------")
 print(f"Winner: {khan}")
 print("--------------------")
 
+poll_winner = (f"--------------------\n" + f"Winner: {khan}\n" + f"--------------------\n")
+out_put = (out_put1 + election_results + poll_winner)
 
 filepathtosave = ("python-challenge/PyPoll/analysis/analysis.txt")
-with open(filepathtosave, 'w', newline="") as text:
-    text.write("Election Results\n")
-    text.write("--------------------\n")
-    text.write(f"Total Votes: {ballot_ct}\n")
-    text.write(f"--------------------\n")
-    text.write(f"{name}: {candidate_info[name]}%  {candidate_count[name]}\n")
-    text.write(f"--------------------\n")
-    text.write(f"Winner: {khan}\n")
-    text.write(f"--------------------\n")
+with open(filepathtosave, 'w') as txt_file:
+    txt_file.write(out_put)
